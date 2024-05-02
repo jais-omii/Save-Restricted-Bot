a091d804f47f4d00970656268ce4b91521a6d705
Om Kumar Jaiswal <oracle.omii@gmail.com>
2024-05-02 11:28:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ import time
 import os
 import threading
 import json
-from os import environ
 
-bot_token = environ.get("TOKEN", "") 
-api_hash = environ.get("HASH", "") 
-api_id = environ.get("ID", "")
+with open('config.json', 'r') as f: DATA = json.load(f)
+def getenv(var): return os.environ.get(var) or DATA.get(var, None)
+
+bot_token = getenv("TOKEN") 
+api_hash = getenv("HASH") 
+api_id = getenv("ID")
 bot = Client("mybot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-ss = environ.get("STRING", "")
+ss = getenv("STRING")
 if ss is not None:
 	acc = Client("myacc" ,api_id=api_id, api_hash=api_hash, session_string=ss)
 	acc.start()
@@ -63,8 +65,8 @@ def progress(current, total, message, type):
 # start command
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/OGxMater")]]), reply_to_message_id=message.id)
+	bot.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, I am Save Restricted Bot, I can send you restricted content by it's post link__\n\n{USAGE}",
+	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Save-Restricted-Bot")]]), reply_to_message_id=message.id)
 
 
 @bot.on_message(filters.text)
@@ -130,8 +132,11 @@ def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
 				except UsernameNotOccupied: 
 					bot.send_message(message.chat.id,f"**The username is not occupied by anyone**", reply_to_message_id=message.id)
 					return
-
-				try: bot.copy_message(message.chat.id, msg.chat.id, msg.id,reply_to_message_id=message.id)
+				try:
+					if '?single' not in message.text:
+						bot.copy_message(message.chat.id, msg.chat.id, msg.id, reply_to_message_id=message.id)
+					else:
+						bot.copy_media_group(message.chat.id, msg.chat.id, msg.id, reply_to_message_id=message.id)
 				except:
 					if acc is None:
 						bot.send_message(message.chat.id,f"**String Session is not Set**", reply_to_message_id=message.id)
@@ -247,16 +252,16 @@ def get_message_type(msg: pyrogram.types.messages_and_media.message.Message):
 
 USAGE = """**FOR PUBLIC CHATS**
 
-**__just send post/s link__**
+__just send post/s link__
 
 **FOR PRIVATE CHATS**
 
-**__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
-then send post/s link__**
+__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
+then send post/s link__
 
 **FOR BOT CHATS**
 
-**__send link with** '/b/', **bot's username and message id, you might want to install some unofficial client to get the id like below__**
+__send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below__
 
 ```
 https://t.me/b/botusername/4321
@@ -264,7 +269,7 @@ https://t.me/b/botusername/4321
 
 **MULTI POSTS**
 
-**__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__**
+__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__
 
 ```
 https://t.me/xxxx/1001-1010
@@ -272,7 +277,7 @@ https://t.me/xxxx/1001-1010
 https://t.me/c/xxxx/101 - 120
 ```
 
-**__note that space in between doesn't matter__**
+__note that space in between doesn't matter__
 """
 
 
